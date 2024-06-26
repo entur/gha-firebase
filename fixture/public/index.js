a5da94c0a5df76cd2fdd9dda45c506be8e92706b
Alexander Brevig <alexander.brevig@hey.com>
2024-06-26 12:41:37 +0200

    fix: read only store

--- fixture/public/index.js
+++ fixture/public/index.js
@@ -12,72 +12,9 @@ const firebaseConfig = {
 };
 
 var db = undefined;
-var user = undefined;
- 
-async function login(email, password) {
-    await firebase.auth().signInWithEmailAndPassword(email, password);
-    user = firebase.auth().currentUser;
-}
  
 function activate() {
-    // document.querySelector("#nav").style.display = "none";
-    // document.querySelector("#journal").style.display = "none";
-    // document.querySelector("#form").style.display = "none";
-    document.querySelector("#date").valueAsDate = new Date();
-    document.querySelector("#login").addEventListener("click", async function () {
-        let email = document.querySelector("#email").value;
-        let password = document.querySelector("#password").value;
-        await login(email, password);
-        await fetch("desc");
-        document.querySelector("#current").innerHTML = user.email;
-        document.querySelector("#email").value = "";
-        document.querySelector("#password").value = "";
-        document.querySelector("#auth").style.display = "none";
-        document.querySelector("#nav").style.display = "block";
-        document.querySelector("#journal").style.display = "block";
-    });
-    document.querySelector("#submit").addEventListener("click", async function () {
-        let date = document.querySelector("#date").value;
-        let description = document.querySelector("#description").value;
-        if(description.trim().length == 0)
-            return;
-        if (new Date(date).getTime() > new Date().getTime())
-            return;
-        await db.collection("journal-entries").add({
-            "description": description,
-            "author": user.email,
-            "date": new Date(date).getTime()
-        });
-        document.querySelector("#date").valueAsDate = new Date();
-        document.querySelector("#description").value = "";
-        await fetch("desc");
-        document.querySelector("#journal").style.display = "block";
-        document.querySelector("#form").style.display = "none";
- 
-    });
- 
-    document.querySelector("#new").addEventListener("click", async function () {
-        document.querySelector("#journal").style.display = "none";
-        document.querySelector("#form").style.display = "block";
-    });
- 
-    document.querySelector("#refresh").addEventListener("click", async function () {
-        await fetch("desc");
-        document.querySelector("#journal").style.display = "block";
-        document.querySelector("#form").style.display = "none";
-    });
- 
-    document.querySelector("#latest").addEventListener("click", async function () {
-        await fetch("desc");
-        document.querySelector("#journal").style.display = "block";
-        document.querySelector("#form").style.display = "none";
-    });
- 
-    document.querySelector("#oldest").addEventListener("click", async function () {
-        await fetch("asc");
-        document.querySelector("#journal").style.display = "block";
-        document.querySelector("#form").style.display = "none";
-    });
+    await fetch("desc");
 }
  
 async function fetch(order) {
